f241fd91425b2169a26ece21ec280716a2520967
success21 <iamsaphal21@gmail.com>
2024-04-13 00:49:24 +0545

    add cloudinary configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,6 +6,9 @@ const _config ={
     databaseUri:process.env.MONGODB_URI,
     env:process.env.NODE_ENV,
     jwtSecret:process.env.JWT_SECRET,
+    cloudApiKey:process.env.CLOUDINARY_API_KEY,
+    cloudApiSecret:process.env.CLOUDINARY_API_SECRET,
+    cloudName:process.env.CLOUDINARY_NAME,
 }
 
 export const config = Object.freeze(_config); 
