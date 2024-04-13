73b4782a820f846687247fa1e9c6e4884d1b9197
success21 <iamsaphal21@gmail.com>
2024-04-13 12:42:54 +0545

    create BookController

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -5,6 +5,9 @@ const BookController ={
     
     createBook: async (req:Request, res:Response, next:NextFunction)=>{
             
+        const files = req.files as{[fieldname:string]:Express.Multer.File[]}
+        console.log('files',files );
+        // console.log(req.files)
         res.status(200).json({
             message: "createBook"
         })
