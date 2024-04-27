44b8948fc9eb197088d1597d3a1a8f5b15e1147d
success21 <iamsaphal21@gmail.com>
2024-04-27 23:26:44 +0545

    add all services template of book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,71 +1,91 @@
-import { NextFunction, Request, Response } from "express"
+import { NextFunction, Request, Response } from "express";
 import { ApiResponse } from "../helper/ApiResponse";
 import createHttpError from "http-errors";
 import CloudinaryService from "../helper/cloudinary";
 import BookModel from "./book.model";
+import bookServices from ".";
+import { User } from "../user/user.types";
 
-
-const BookController ={
-    
-    createBook: async (req:Request, res:Response, next:NextFunction)=>{
-
-        try{
-            
-        const {genre,  title} = req.body;
-
-        if([genre,title].some(field=>field===undefined || field.trim()==='')){
-            
-            next(createHttpError(400,'All field are required.'));
-        }
-
-        const files = req.files as{[fieldname:string]:Express.Multer.File[]}
-      
-        // if (
-        //     req.files &&
-        //     Array.isArray(req.files.coverImage) &&
-        //     req.files.coverImage.length > 0
-        //   ) {
-        //     coverImageLocalPath = req.files.coverImage[0].path;
-        //   }
-
-        const coverImagePath = files?.coverImage[0]?.path;
-        const filePath = files?.file[0]?.path;
-
-        if(!coverImagePath || !filePath){
-            next(createHttpError(400, 'Cover image & file are required.'));
-        }
-        const coverImage =await CloudinaryService.uploadImage(coverImagePath);
-        const file = await CloudinaryService.uploadImage(filePath);
-
-        if(!coverImage || !file){
-            next(createHttpError(500, 'Error uploading cover Image or file.'));
-        }
-
-      const createdBook=  await BookModel.create({
-            genre,
-            title,
-            coverImage:coverImage?.secure_url,
-            file:file?.secure_url
-        })
-
-        if(!createdBook){
-            next(createHttpError(500, 'Error creating book.'));
-        }
-        
-     
-
-        res.status(201).json( new ApiResponse(201, createdBook, 'Book created successfully.' ));
-    }catch(err){
-
-        if(err instanceof Error){
-            next(createHttpError(500, err.message));
-        }
-        next(createHttpError(500, 'Error creating book.'));
-    }
-    
-    },
-
-   
-
+interface UserRequest extends Request {
+  user?: User | null;
 }
+
+const BookController = {
+  createBook: async (req: UserRequest, res: Response, next: NextFunction) => {
+    try {
+      const { genre, title } = req.body;
+
+      if (
+        [genre, title].some(
+          (field) => field === undefined || field.trim() === ""
+        )
+      ) {
+        next(createHttpError(400, "All field are required."));
+      }
+
+      const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+
+      const coverImagePath = files?.coverImage[0]?.path;
+      const filePath = files?.file[0]?.path;
+
+      if (!coverImagePath || !filePath) {
+        next(createHttpError(400, "Cover image & file are required."));
+      }
+      const coverImage = await CloudinaryService.uploadImage(coverImagePath);
+      const file = await CloudinaryService.uploadImage(filePath);
+
+      if (!coverImage || !file) {
+        next(createHttpError(500, "Error uploading cover Image or file."));
+      }
+
+      const createdBook = await bookServices.createBook({
+        genre,
+        title,
+        coverImage: coverImage?.secure_url!,
+        file: file?.secure_url!,
+      });
+
+      if (!createdBook) {
+        next(createHttpError(500, "Error creating book."));
+      }
+
+      res
+        .status(201)
+        .json(new ApiResponse(201, createdBook, "Book created successfully."));
+    } catch (err) {
+      if (err instanceof Error) {
+        next(createHttpError(500, err.message));
+      }
+      next(createHttpError(500, "Error While creating book."));
+    }
+  },
+  deleteBookById: async (req: UserRequest, res: Response) => {
+    try {
+      const { id } = req.params;
+
+      //businessLogin
+      const toBeDeletedBook = await BookModel.findById(id);
+      if (!toBeDeletedBook) {
+        return res
+          .status(404)
+          .json(new ApiResponse(404, null, "Book not found."));
+      }
+      if (toBeDeletedBook.author === req.user) {
+        return res
+          .status(403)
+          .json(
+            new ApiResponse(
+              403,
+              null,
+              "You are not authorized to delete this book."
+            )
+          );
+      }
+
+      res
+        .status(200)
+        .json(new ApiResponse(200, null, "Book deleted successfully."));
+    } catch (error) {}
+  },
+};
 export default BookController;
--- src/book/book.services.ts
+++ src/book/book.services.ts
@@ -1,11 +1,61 @@
-
 // interface CreateUserOptions {
 //   name: string;
 //   email: string;
 //   password: string;
-// }
-class UserServices {
-//   constructor(private readonly userModel: typeof UserModel) {}
-  
+
+interface CreateBookOptions {
+  genre: string;
+  title: string;
+  coverImage: string;
+  file: string;
 }
-export default UserServices;
+import BookModel from "./book.model";
+
+// }
+class BookService {
+  constructor(private readonly bookModel: typeof BookModel) {}
+
+  async createBook(options: CreateBookOptions) {
+    try {
+      const book = await this.bookModel.create(options);
+      return book;
+    } catch (err) {
+      throw new Error("Error creating book");
+    }
+  }
+  async deleteBookById(id:string){
+    try {
+      const book = await this.bookModel.findByIdAndDelete(id);
+      return book;
+    } catch (err) {
+      throw new Error("Error deleting book");
+    }
+  }
+  async getAllBooks() {
+    try {
+      const books = await this.bookModel.find();
+      return books;
+    } catch (err) {
+      throw new Error("Error getting books");
+    }
+  }
+  async getBookById(id:string){
+    try {
+      const book = await this.bookModel.findById(id);
+      return book;
+    } catch (err) {
+      throw new Error("Error getting book");
+    }
+  }
+  async updateBookById(id:string,options:CreateBookOptions){
+    try {
+      const book = await this.bookModel.findByIdAndUpdate(id,options,{new:true});
+      return book;
+    } catch (err) {
+      throw new Error("Error updating book");
+    }
+  }
+
+
+}
+export default BookService;
