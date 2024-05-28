6f7d9fc1babef3fbef8009b8bbefe8ba8bde3de4
success21 <iamsaphal21@gmail.com>
2024-05-28 19:53:36 +0545

    setup shadcn ui

--- elib-dashboard/src/App.tsx
+++ elib-dashboard/src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
       <h1>Welcome elib-dashboard</h1>
+      <Button>Go to dashboard</Button>
     </>
   )
 }
